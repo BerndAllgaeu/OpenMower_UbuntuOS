b9a1d99fc247560fc3a6729a94f4af1c5fe21c6e
BerndAllgaeu <bernd@happach.org>
2025-07-09 07:56:03 +0200

    cleanup

--- build/README.md
+++
@@ -1,6 +0,0 @@
-# Build-Skripte
-
-Hier liegen alle Skripte und Hilfsdateien für den Image-Build-Prozess.
-
-- `build-image.sh`: Erstellt das Ubuntu-Image für den Raspberry Pi
-- Weitere Skripte folgen
--- build/build-image.sh
+++ build/build-image.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 # OpenMower_UbuntuOS: Setup-Skript für Raspberry Pi (ausführen nach erstem Boot)
 # Dieses Skript installiert Ansible, klont das Setup-Repo und führt das Playbook lokal aus.
 
-REPO_URL="https://github.com/<dein-github-user>/<repo>.git"  # <-- Hier anpassen!
-PLAYBOOK="playbook.yml"  # Name des Playbooks im Repo
+REPO_URL="https://github.com/BerndAllgaeu/OpenMower_UbuntuOS.git"  # <-- Hier anpassen!
+PLAYBOOK="build/playbook.yml"  # Name des Playbooks im Repo
 REPO_DIR="OpenMower_UbuntuOS-setup"  # Zielverzeichnis für das geklonte Repo
 
 # 1. Ansible & Git installieren (falls nicht vorhanden)
@@ -16,6 +16,9 @@ sudo apt-get install -y ansible git
 rm -rf "$REPO_DIR"
 git clone "$REPO_URL" "$REPO_DIR"
 cd "$REPO_DIR"
+
+# Locale-Workaround für Ansible
+export LC_ALL=C.UTF-8
 
 # 3. Playbook lokal ausführen
 ansible-playbook -i "localhost," -c local "$PLAYBOOK"
